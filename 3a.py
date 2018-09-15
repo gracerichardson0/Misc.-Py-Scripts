6ccb4fb833683f8020766c3d419add4277499df0
nsrichardson <35121234+nsr96@users.noreply.github.com>
2018-09-15 00:01:22 -0400

    Initial commit

---
+++ 3a.py
@@ -0,0 +1,7 @@
+number = int(input ("Enter a number:"))
+if number <=10:
+    print ("Your number was 10 or less")
+else :
+    print ("Your number was greater than 10")
+
+
---
+++ letterA.py
@@ -0,0 +1,8 @@
+letterA = (input("Enter the letter A:"))
+if letterA == "A" or "a":
+    print ("Thanks for the A!")
+else:
+    print ("Please follow the directions next time")
+
+
+
